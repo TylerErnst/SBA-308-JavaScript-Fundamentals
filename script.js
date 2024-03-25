44f63fb40b1e9e0707291ae78dee7487df20f94a
TylerErnst <tcernst5@gmail.com>
2024-03-25 14:31:16 -0600

    Created list of students

--- script.js
+++ script.js
@@ -78,20 +78,63 @@ const CourseInfo = {
   
   function getLearnerData(course, ag, submissions) {
     // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
-      }
-    ];
+
+    let result = [];
+    // the ID of the learner for which this data has been collected
+ //   "id": number,
+    
+    //create student list
+    let studentList = [];
+    submissions.forEach(submission =>{
+        studentList.push(submission.learner_id);
+    });
+    console.log(studentList)
+
+    //Filter duplicates
+    studentList = studentList.filter( function( item, index, inputArray ) {
+        return inputArray.indexOf(item) == index;
+    });
+
+    console.log(studentList)
+
+    //fill results with students
+    studentList.forEach((student, i) => {
+        let newStudent = {};
+        newStudent.id = studentList[i];
+        result.push(newStudent);
+    })
+    console.log(result)
+
+
+    
+    
+    // the learner’s total, weighted average, in which assignments
+    // with more points_possible should be counted for more
+    // e.g. a learner with 50/100 on one assignment and 190/200 on another
+    // would have a weighted average score of 240/300 = 80%.
+ //   "avg": number,
+    // each assignment should have a key with its ID,
+    // and the value associated with it should be the percentage that
+    // the learner scored on the assignment (submission.score / points_possible)
+//    <assignment_id>: number,
+    // if an assignment is not yet due, it should not be included in either
+    // the average or the keyed dictionary of scores
+
+
+    // result = [
+    //   {
+    //     id: 125,
+    //     avg: 0.985, // (47 + 150) / (50 + 150)
+    //     1: 0.94, // 47 / 50
+    //     2: 1.0 // 150 / 150
+    //   },
+    //   {
+    //     id: 132,
+    //     avg: 0.82, // (39 + 125) / (50 + 150)
+    //     1: 0.78, // 39 / 50
+    //     2: 0.833 // late: (140 - 15) / 150
+    //   }
+    // ];
   
     return result;
   }
