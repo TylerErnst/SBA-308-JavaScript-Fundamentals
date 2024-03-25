16c6b9c12950787f2c82fc7531f91cf2c0922220
TylerErnst <tcernst5@gmail.com>
2024-03-25 17:49:19 -0600

    added list of requirements

--- script.js
+++ script.js
@@ -166,6 +166,8 @@ function getLearnerData(course, ag, submissions) {
             }
           }
         });
+
+        //Get average score
         try {
           if (totalScore === 0) {
             throw new Error("Points possible cannot be 0");
@@ -229,3 +231,23 @@ function getLearnerData(course, ag, submissions) {
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 console.log(result);
+
+
+
+
+
+
+// Declare variables properly using let and const where appropriate.
+// Use operators to perform calculations on variables and literals.
+// Use strings, numbers, and Boolean values cached within variables.
+// Use at least two if/else statements to control program flow. Optionally, use at least one switch statement.
+// Use try/catch statements to manage potential errors in the code, such as incorrectly formatted or typed data being fed into your program.
+// Utilize at least two different types of loops.
+// Utilize at least one loop control keyword such as break or continue.
+// Create and/or manipulate arrays and objects.
+// Demonstrate the retrieval, manipulation, and removal of items in an array or properties in an object.
+// Use functions to handle repeated tasks.
+// Program outputs processed data as described above. Partial credit will be earned depending on the level of adherence to the described behavior.
+// Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+// Commit frequently to the git repository.
+// Include a README file that contains a description of your application.
